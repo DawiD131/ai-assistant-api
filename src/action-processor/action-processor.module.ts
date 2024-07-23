15f9a521e25d279be37d20ffd559b290732ee393
dawidkutwin <dawid.kutwin200@gmail.com>
2024-07-24 01:00:44 +0200

    Fix bad modules dependencies

--- src/action-processor/action-processor.module.ts
+++ src/action-processor/action-processor.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ActionProcessorService } from './action-processor.service';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { OpenaiService } from '../open-ai/openai.service';
 
 @Module({
-  imports: [EventEmitter2],
   providers: [ActionProcessorService, OpenaiService],
 })
 export class ActionProcessorModule {}
--- src/entry/entry.module.ts
+++ src/entry/entry.module.ts
@@ -5,10 +5,8 @@ import { PrismaService } from '../services/prisma.service';
 import { OpenaiService } from '../open-ai/openai.service';
 import { MessageRepository } from '../conversation/message/message.repository';
 import { ConversationRepository } from '../conversation/conversation/conversation.repository';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [EventEmitter2],
   controllers: [EntryController],
   providers: [
     EntryService,
