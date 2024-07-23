eff76992b424fafc24fec6295e7e548034905f0f
dawidkutwin <dawid.kutwin200@gmail.com>
2024-07-24 00:25:22 +0200

    up

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://luaai.byst.re/',
+    origin: 'https://luaai.byst.re',
     credentials: true,
   });
 
